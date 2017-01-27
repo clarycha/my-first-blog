3a60efb50091d3c13d73c34f426b5779e3681d8a
clarycha <clarycha01214@gmail.com>
2017-01-27 11:32:50 +0900

    My Django, First commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default = timezone.now)
+	published_date = models.DateTimeField(blank = True, null = True)
+
+	def publidh(self):
+		self.published_date = timezone.now()
+		sel.save()
+
+	def __str__(self):
+		return self.title
+
+# Create your models here.
